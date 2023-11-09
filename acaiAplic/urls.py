9ff86b9939c877ad9878215a9727ead514cda472
PedroABarros <pedrokii.a.b@gmail.com>
2023-11-09 16:07:31 -0300

    Fazendo a parte do HTML

--- acaiAplic/urls.py
+++ acaiAplic/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import IndexView
+from .views import EntrarView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path('entrar/', EntrarView.as_view(), name='entrar'),
 ]
+
--- acaiAplic/views.py
+++ acaiAplic/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class EntrarView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'entrar.html')
