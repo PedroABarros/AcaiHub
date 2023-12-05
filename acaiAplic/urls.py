37b99a345c5bb781ea72dd671030e292f2661d6e
PedroABarros <pedrokii.a.b@gmail.com>
2023-12-05 19:08:45 -0300

    finalização do trabalho"

--- acaiAplic/urls.py
+++ acaiAplic/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, lista_produtos, montar_pedido, finalizar_pedido
 from .views import EntrarView
 from acaiAplic import views
-from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path('entrar/', EntrarView.as_view(), name='entrar'),
     path('registro/', views.register_page, name='registro'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('montar_pedido/', montar_pedido, name='montar_pedido'),
+    path('lista_produtos/', lista_produtos, name='lista_produtos'),
+    path('final_pedido/', finalizar_pedido, name='finalizar_pedido'),
+]
 
--- acaiAplic/views.py
+++ acaiAplic/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from urllib import request
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import TemplateView
@@ -5,9 +7,16 @@ from acaiAplic.forms import ProdutoForm
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 
+from acaiAplic.models import Produto
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['produtos'] = Produto.objects.all()
+        return context
 
 class EntrarView(View):
     def get(self, request, *args, **kwargs):
@@ -15,6 +24,22 @@ class EntrarView(View):
 
 def register_page(request):
     return render(request, 'registro.html')
+
+def montar_pedido(request):
+    return render(request, 'base.html', {})
+
+def finalizar_pedido(request):
+    return render(request, 'finalPedido.html', {})
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+
+    query = request.GET.get('q')
+    if query:
+        produtos = produtos.filter(nome_produto__icontains=query)
+
+    return render(request, 'sua_template.html', {'produtos': produtos})
+
 
 
 
